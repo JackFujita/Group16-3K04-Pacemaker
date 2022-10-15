5c88fb9e840c3b7b0f1ded6fff68eb92adc89e81
Jack <fujitaj@mcmaster.ca>
2022-10-14 20:39:49 -0400

    CSV

--- LoginTest.py
+++ LoginTest.py
@@ -1,13 +1,37 @@
 from tkinter import *
 from functools import partial
 import numpy as np
+import csv
 
+header = ['Username', 'Password']
+data1 = [
+	['Jack','password123'],
+	['Shu','password789']]
+
+with open("Secrets.csv", 'w', newline='') as file:
+	writer = csv.writer(file)
+	writer.writerow(header)
+	writer.writerows(data1)
+	writer.writerow(['Tim','dad'])
+
+
+
+secret_array = np.empty([2, 2], dtype=str)
 
 def validateLogin(username, password):
 	input_username = username.get()
 	input_password = password.get()
 	print("username entered :", username.get())
 	print("password entered :", password.get())
+
+
+	with open('test.npy', 'wb') as f:
+		np.save(f, np.array([1, 2]))
+		np.save(f, np.array([1, 3]))
+	with open('test.npy', 'rb') as f:
+		a = np.load(f)
+		b = np.load(f)
+	print(a, b)
 
 
 	f = open("secrets.txt", "a")
