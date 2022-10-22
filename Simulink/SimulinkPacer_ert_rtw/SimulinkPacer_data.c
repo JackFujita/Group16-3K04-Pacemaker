4affffccf5f336b131d79eaa21bec49fbde1508f
Sabrina Sousa <sabrinavsousa14@gmail.com>
2022-10-22 17:54:19 -0400

    Fixed errors in AAI and VVI

---
+++ Simulink/SimulinkPacer_ert_rtw/SimulinkPacer_data.c
@@ -0,0 +1,104 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: SimulinkPacer_data.c
+ *
+ * Code generated for Simulink model 'SimulinkPacer'.
+ *
+ * Model version                  : 5.43
+ * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
+ * C/C++ source code generated on : Sat Oct 22 17:44:23 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "SimulinkPacer.h"
+
+/* Block parameters (default storage) */
+P_SimulinkPacer_T SimulinkPacer_P = {
+  /* Expression: SampleTime
+   * Referenced by: '<S3>/Digital Read'
+   */
+  -1.0,
+
+  /* Expression: SampleTime
+   * Referenced by: '<S3>/Digital Read1'
+   */
+  -1.0,
+
+  /* Expression: 20
+   * Referenced by: '<S2>/Constant1'
+   */
+  20.0,
+
+  /* Expression: 20
+   * Referenced by: '<S2>/Constant3'
+   */
+  20.0,
+
+  /* Expression: 75
+   * Referenced by: '<S2>/Constant4'
+   */
+  75.0,
+
+  /* Expression: 75
+   * Referenced by: '<S2>/Constant2'
+   */
+  75.0,
+
+  /* Expression: 60
+   * Referenced by: '<S2>/lower rate limit in pulses//min'
+   */
+  60.0,
+
+  /* Expression: 60000
+   * Referenced by: '<S2>/min to ms'
+   */
+  60000.0,
+
+  /* Expression: 120
+   * Referenced by: '<S2>/upper rate limit in pulses//min'
+   */
+  120.0,
+
+  /* Expression: 60000
+   * Referenced by: '<S2>/min to ms1'
+   */
+  60000.0,
+
+  /* Expression: 3
+   * Referenced by: '<S2>/Constant6'
+   */
+  3.0,
+
+  /* Expression: 320
+   * Referenced by: '<S2>/Constant5'
+   */
+  320.0,
+
+  /* Expression: 60
+   * Referenced by: '<S2>/Constant'
+   */
+  60.0,
+
+  /* Expression: 250
+   * Referenced by: '<S2>/Constant9'
+   */
+  250.0,
+
+  /* Expression: 60
+   * Referenced by: '<S2>/Constant7'
+   */
+  60.0
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
